74bec7b94a074b7402fdcea2bcaa360874edc95e
n.melihova <melihova.qa@gmail.com>
2023-03-26 17:35:37 +0200

    Final version

--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -8,17 +8,6 @@ const inviteeDashboardPage = require("../fixtures/pages/inviteeDashboardPage.jso
 import { faker } from "@faker-js/faker";
 
 describe("user can create a box and run it", () => {
-  //пользователь 1 логинится
-  //пользователь 1 создает коробку
-  //пользователь 1 получает приглашение
-  //пользователь 2 переходит по приглашению
-  //пользователь 2 заполняет анкету
-  //пользователь 3 переходит по приглашению
-  //пользователь 3 заполняет анкету
-  //пользователь 4 переходит по приглашению
-  //пользователь 4 заполняет анкету
-  //пользователь 1 логинится
-  //пользователь 1 запускает жеребьевку
   let newBoxName = faker.word.noun({ length: { min: 5, max: 10 } });
   let wish1 = faker.word.adjective() + faker.word.noun();
   let maxAmount = 50;
